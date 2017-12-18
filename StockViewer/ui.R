a20510eccaa9ad15f9766eb537a553b77ed08f4b
Martin Rodriguez <m.rodriguezf@alum.up.edu.pe>
2017-12-17 19:08:48 -0500

    Added UI and fixed server side csv creation for getFinancials data #9 #10

--- StockViewer/ui.R
+++ StockViewer/ui.R
@@ -21,6 +21,14 @@ fluidPage(theme = shinytheme("flatly"),
                label = 'Date Range: Year - Month - Day',
                start = Sys.Date() - 366, end = Sys.Date() - 1, min = "2007-01-01", max = Sys.Date(), startview = "decade"),
                
+               splitLayout(helpText("Download Quarterly Data"), helpText("Download Annual Data")),
+               splitLayout(downloadButton("downloadISQ", "Income Statement"), downloadButton("downloadISA", "Income Statement")),
+               splitLayout(downloadButton("downloadBSQ", "Balance Sheet"), downloadButton("downloadBSA", "Balance Sheet")),
+               splitLayout(downloadButton("downloadCFQ", "Cash Flow"), downloadButton("downloadCFA", "Cash Flow")),
+               
+               helpText("Other Data"),
+               splitLayout(downloadButton("dividends", "Dividends"), downloadButton("stock_price", "Prices"), downloadButton("splits", "Splits")),
+               
                helpText("Source: Yahoo Finance")
                
                ),
