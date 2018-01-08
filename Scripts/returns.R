f4ebdb7f1242f8c26ec5007c25eccdd9428922c2
Martin Rodriguez <m.rodriguezf@alum.up.edu.pe>
2018-01-07 21:02:18 -0500

    added script showing how to get daily returns

---
+++ Scripts/returns.R
@@ -0,0 +1,23 @@
+library(scales)
+library(tidyverse)
+library(quantmod)
+
+
+
+share <- getSymbols("SNAP", auto.assign = F)
+
+returns <- share[,6]
+returns <- ((returns[,1] - returns[[1]]) / returns[[1]])
+
+autoplot(returns) + 
+  theme(panel.background = element_rect(fill = "white", colour = "white"), 
+        panel.grid.major.y = element_line(colour = rgb(195/255, 195/255, 195/255, alpha = 0.5), linetype = "solid"), 
+        plot.background = element_rect(fill = "white", colour = "white"), 
+        text = element_text(size = 12, family = "Lato", color = rgb(44/255, 62/255, 80/255)), 
+        plot.title = element_text(face = "bold", size = 18),
+        axis.ticks.y = element_blank()) + 
+  labs(x = "", y = "") + geom_area(fill = "#0066B2", alpha = 0.3) + geom_line(size = 1, col = "#0066B2") + 
+  scale_y_continuous(position = "right",
+                     labels = percent)
+
+
